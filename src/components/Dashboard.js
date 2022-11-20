2ece0d866523cd83690407152c79206445e896de
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-11-19 21:07:50 -0500

    fix styles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import OpenConversation from './OpenConversation'
-import Sidebar from './Sidebar'
-import { useConversations } from './contexts/ConversationsProvider'
+import React from "react";
+import OpenConversation from "./OpenConversation";
+import Sidebar from "./Sidebar";
+import { useConversations } from "./contexts/ConversationsProvider";
+import "./css/dashboard.css";
 
-export default function Dashboard({id}) {
-  const {selectedConversation} = useConversations()
-  return (<>
-          <Sidebar id={id}/>
-          { selectedConversation && <OpenConversation />}
-   </>
-  )
+export default function Dashboard({ id }) {
+  const { selectedConversation } = useConversations();
+  return (
+    <div className="flex">
+      <Sidebar id={id} />
+      {selectedConversation && <OpenConversation />}
+    </div>
+  );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,44 +1,63 @@
-import React, {useState} from 'react'
-import './css/sidebar.css'
-import Conversations from './Conversations';
-import Contacts from './Contacts';
-import NewContactModal from './NewContactModal';
-import NewConversationModal from './NewConversationModal';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faAddressBook, faMessage, faUserPlus, faComments} from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import "./css/sidebar.css";
+import Conversations from "./Conversations";
+import Contacts from "./Contacts";
+import NewContactModal from "./NewContactModal";
+import NewConversationModal from "./NewConversationModal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressBook,
+  faMessage,
+  faUserPlus,
+  faComments,
+} from "@fortawesome/free-solid-svg-icons";
 
+export default function Sidebar({ id }) {
+  const [leftTabOn, setLeftTabOn] = useState(true);
+  const conversationsOpen = leftTabOn ? (
+    <FontAwesomeIcon icon={faComments} />
+  ) : (
+    <FontAwesomeIcon icon={faUserPlus} />
+  );
+  const [isModalOpened, setIsModalOpened] = useState(false);
 
-export default function Sidebar({id}) {
-    // const  CONVERSATIONS_KEY = 'conversations';
-    // const  CONTACTS_KEY = 'contacts';
-    const [leftTabOn, setLeftTabOn] = useState(true);
-    const conversationsOpen = leftTabOn ? <FontAwesomeIcon icon={faComments} /> : <FontAwesomeIcon icon={faUserPlus} />;
-    const [isModalOpened, setIsModalOpened] = useState(false);
-    
   return (
-    <>
-    <div className='sidebar-container'>
-        <div className="tabs">
-          <button onClick={() => setLeftTabOn(true)} className='active'> <FontAwesomeIcon icon={faMessage} /></button>
-          <button onClick={() => setLeftTabOn(false)} > <FontAwesomeIcon icon={faAddressBook} /></button>            
-        </div>
+    <div className="sidebar-container">
+      <div className="tabs">
+        <button onClick={() => setLeftTabOn(true)} className="active">
+          {" "}
+          <FontAwesomeIcon icon={faMessage} />
+        </button>
+        <button onClick={() => setLeftTabOn(false)}>
+          {" "}
+          <FontAwesomeIcon icon={faAddressBook} />
+        </button>
+      </div>
 
-        <div className='content'>
-          {leftTabOn ? <Conversations/> : <Contacts />}
-        </div>
+      <div className="content">
+        {leftTabOn ? <Conversations /> : <Contacts />}
+      </div>
 
-        <div className='contact-id'>
-          Your Id: {id}
-        </div>
-        <button className='action-btn' onClick={() =>setIsModalOpened(true)}>{conversationsOpen}</button>  
+      <div className="contact-id">Your Id: {id}</div>
+      <button className="action-btn" onClick={() => setIsModalOpened(true)}>
+        {conversationsOpen}
+      </button>
+
+      <div>
+        {leftTabOn ? (
+          <NewConversationModal
+            trigger={isModalOpened}
+            setTrigger={setIsModalOpened}
+            className="modal"
+          />
+        ) : (
+          <NewContactModal
+            trigger={isModalOpened}
+            setTrigger={setIsModalOpened}
+            className="modal"
+          />
+        )}
+      </div>
     </div>
-
-    <div>
-        {leftTabOn ? 
-        <NewConversationModal trigger={isModalOpened} setTrigger={setIsModalOpened} className='modal'/> :
-        <NewContactModal trigger={isModalOpened} setTrigger={setIsModalOpened} className='modal'/>}
-    </div>     
-    
-    </>
-  )
+  );
 }
